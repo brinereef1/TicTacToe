25205e8c0aa2eb0f961a38bd9d2fcaf59f639d79
Brine <santra.brine2020@gmail.com>
2026-09-01 12:48:36 +0530

    applying event system

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,8 +1,16 @@
+using System;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
+
+    public event EventHandler<OnClickedOnPositionEventArgs> onClickedOnGridPosition;
+    public class OnClickedOnPositionEventArgs : EventArgs
+    {
+        public int x;
+        public int y;
+    }
 
     private void Awake()
     {
@@ -12,13 +20,17 @@ public class GameManager : MonoBehaviour
             Destroy(gameObject);
             return;
         }
-
         Instance = this;
     }
 
     public void ClickedOnGridPosition(int x, int y)
     {
         Debug.Log("ClickedOnGridPosition: " + x + " " + y);
+        onClickedOnGridPosition?.Invoke(this, new OnClickedOnPositionEventArgs
+        {
+            x = x,
+            y = y
+        });
     }
 
     private void OnDestroy()
--- Assets/Scripts/GridPosition.cs
+++ Assets/Scripts/GridPosition.cs
@@ -6,20 +6,9 @@ public class GridPosition : MonoBehaviour
     [SerializeField] private int x;
     [SerializeField] private int y;
 
-    private void Update()
+    void OnMouseDown()
     {
-        if (Mouse.current != null && Mouse.current.leftButton.wasPressedThisFrame)
-        {
-            Vector2 mousePos = Mouse.current.position.ReadValue();
-            Vector2 worldPos = Camera.main.ScreenToWorldPoint(mousePos);
-            Collider2D hit = Physics2D.OverlapPoint(worldPos);
-
-            if (hit != null)
-            {
-                Debug.Log(hit.name); 
-                GameManager.Instance.ClickedOnGridPosition(x, y);
-            }
-                
-        }
+        Debug.Log("Clicked " + x + " " + y);
+        GameManager.Instance.ClickedOnGridPosition(x, y);
     }
 }
